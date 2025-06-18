8fe427a9c7bb1df2d204ed625ccaca15cf51f983
flb <13703937803@163.com>
2025-06-18 09:39:54 +0800

    fix: 修复路由参数展示逻辑，使用计算属性优化代码

--- src/views/demo/route-params.vue
+++ src/views/demo/route-params.vue
@@ -3,14 +3,14 @@ defineOptions({
   name: 'RouteParamsDemo',
 })
 
-const str =
-  Object.keys(useRoute().query || {}).length > 0
-    ? `路由参数: ${JSON.stringify(useRoute().query)}`
-    : '无路由参数'
+const computedStr = computed(() => {
+  const query = useRoute().query || {}
+  return Object.keys(query).length > 0 ? `路由参数: ${JSON.stringify(query)}` : '无路由参数'
+})
 </script>
 
 <template>
-  <div>{{ str }}</div>
+  <div>{{ computedStr }}</div>
 </template>
 
 <style lang="scss" scoped></style>
